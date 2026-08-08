cba71890be095ee3a8db9b800ec352e99bf0ed13
Ayush Sahu <ayush.9sahu@gmail.com>
2026-08-08 23:38:38 +0530

    Refactor MongoDB connection string to use env variable
    Updated MONGO_DETAILS to use environment variable for MongoDB URL.

--- source/db_helpers/db_connection.py
+++ source/db_helpers/db_connection.py
@@ -1,8 +1,11 @@
+import os
 import motor.motor_asyncio
 
-
-MONGO_DETAILS = "mongodb://matchingproject:matchingproject@mongodb"
+MONGO_DETAILS = os.getenv(
+    "MONGODB_URL",
+    "mongodb://localhost:27017"
+)
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
-database = client['matchingdb']
+database = client["matchingdb"]
